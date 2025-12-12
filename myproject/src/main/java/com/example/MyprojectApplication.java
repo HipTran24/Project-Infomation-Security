ddf24e8de6aadbbcfe042ccdbf09f1d73f6e7cb7
myproject <tranthanhhiep8125@gmail.com>
2025-12-12 22:30:11 +0700

    Mã hoá ssl/tsl update https

--- myproject/src/main/java/com/example/MyprojectApplication.java
+++ myproject/src/main/java/com/example/MyprojectApplication.java
@@ -6,7 +6,6 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
-@ComponentScan(basePackages = {"com.example"})  // quét toàn bộ com.example.*
 public class MyprojectApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(MyprojectApplication.class, args);
